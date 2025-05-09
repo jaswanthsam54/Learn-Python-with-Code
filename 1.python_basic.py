01bcfce2ef8b35d4bfc9ba4331d1e4aa2a48f484
jaswanthsam54 <saminepallyjaswanth@gmail.com>
2025-05-09 13:40:07 +0530

    Add files via upload

---
+++ 1.python_basic.py
@@ -0,0 +1,24 @@
+# Python is a high level porgramming language invented by GUIDO VAN ROSSUM IN 1980
+# Python is an interpreted , compiled  language and Dynamic
+
+# First PYTHON PROGRAM
+print("hello") # using double quotes  is valid 
+print('hello') # using single quote is also valid 
+
+# NOTE : Using double quote and single in the same line is invalid and causes an error kk
+
+#input function in python 
+#input function is like scanf() in c language
+
+#Demo program 
+a = input("Enter your name :")
+print(a)
+#output :Enter your name :Jaswanth
+#Jaswanth 
+#the above case is valid only for strings
+
+#to get a number value whether its float or int we use the below format
+a = int(input("Enter your age:"))
+print(a)
+a = float(input("Enter your age:"))
+print(a)
